e9667944ea44913f048ce39ca0f50117e566e76d
Hannes Mehnert <hannes@mehnert.org>
2026-01-08 12:49:05 +0100

    put generated-osv into a specific branch without advisories subdirectory

--- bin/advisories.ml
+++ bin/advisories.ml
@@ -848,7 +848,7 @@ let to_osv { header ; summary ; details } =
       let stop = String.rindex id '-' in
       String.sub id start (stop - start)
     in
-    let osv = Some (base_url ^ "osv/advisories/" ^ year ^ "/" ^ id ^ ".json") in
+    let osv = Some (base_url ^ "generated-osv/" ^ year ^ "/" ^ id ^ ".json") in
     let human_link = Some (base_url ^ "main/advisories/" ^ year ^ "/" ^ id ^ ".md") in
     Some Json.{ osv ; human_link }
   in
